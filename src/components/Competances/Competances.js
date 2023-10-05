88f53320dd96883abf3d3ea1fc8fc66b773074c3
NesrineYAH <nesrineyahoum@gmail.com>
2023-10-05 15:09:33 +0200

    //

--- src/components/Competances/Competances.js
+++ src/components/Competances/Competances.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Presentation from "../Presentation/presentation";
-import Banner from "../Banner/Banner";
 
 const Competance = () => {
   return (
     <div>
-      <Banner />
       <Presentation />
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Banner from "../../components/Banner/Banner";
 import Form from "../../components/Form/Form";
 import { Routes, Route } from "react-router-dom";
 import Presentation from "../../components/Presentation/presentation";
@@ -9,7 +8,7 @@ import Info from "../../components/Infos/Info";
 const Home = () => {
   return (
     <div className="home">
-      <Banner />
+
 
       <Info />
       <Form />
