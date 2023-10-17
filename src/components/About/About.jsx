6599291e3a251916f01b7c714d754b81d83afa8e
NesrineYAH <nesrineyahoum@gmail.com>
2023-10-18 00:33:03 +0200

    //

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,161 @@
-import React from "react";
-import imagePortfolio from "../../assets/images/bakground/Image Portfolio.png";
+import React, { Component } from 'react';
 
-const About = () => {
-  return (
-    <div id="About">
-      <div className="About__info">
-      <h1 className="monTitre">NESRINE BEKKAR YAHOUM </h1>
-        <h2 className="monTitre">Développeuse web Frontend</h2>   
-   <button className="About__btn" link to= "/#Profil">
-  plus d'infos
-   </button>
-   
-   <br/>
 
+class TxtRotate extends Component {
+    constructor(props) {
+      super(props);
   
-      </div>
-     
-    </div>
-  );
-};
+      this.state = {
+        toRotate: JSON.parse(props.toRotate),
+        loopNum: 0,
+        txt: '',
+        isDeleting: false,
+      };
+    }
+  
+    componentDidMount() {
+      setTimeout(() => {
+      this.tick();
+      }, 3000)
+    }
+  
+    tick() {
+      const { toRotate, loopNum, txt, isDeleting } = this.state;
+      const i = loopNum % toRotate.length;
+      const fullTxt = toRotate[i];
+  
+      if (isDeleting) {
+        this.setState({ txt: fullTxt.substring(0, txt.length - 1) });
+      } else {
+        this.setState({ txt: fullTxt.substring(0, txt.length + 1) });
+      }
+  
+      let delta = 300 - Math.random() * 100;
+      const that = this;
+  
+      if (isDeleting) {
+        delta /= 2;
+      }
+  
+      if (!isDeleting && txt === fullTxt) {
+        this.setState({ isDeleting: true });
+        delta = this.props.period;
+      } else if (isDeleting && txt === '') {
+        this.setState(prevState => ({
+          isDeleting: false,
+          loopNum: prevState.loopNum + 1,
+          txt: '',
+        }));
+        delta = 1000;
+      }
+      setTimeout(() => {
+        that.tick();
+      }, delta);
+    }
+  
+  render() {
+    return (
+      <span className="txt-rotate">
+        <span className="wrap">{this.state.txt}</span>
+      </span>
+    );
+  }
+}
 
-export default About;
+class TxtToDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      h1Text: '',
+      h2Text: '',
+    };
+
+    this.h1Ref = React.createRef();
+    this.h2Ref = React.createRef();
+  }
+
+  typeEffect(text, speed, setStateKey) {
+    let j = 0;
+  
+    let timer = setInterval(() => {
+      if (j < text.length) {
+        const lastChar = text.charAt(j);
+        console.log(lastChar);
+        this.setState(prevState => ({
+          [setStateKey]: prevState[setStateKey].slice(0, j-1) + lastChar,
+        }));
+        j+=1;
+      } else {
+        clearInterval(timer);
+      }
+    }, speed);
+  }
+
+  componentDidMount() {
+    const speed = 100;
+    const h1 = this.h1Ref.current;
+    const h2 = this.h2Ref.current;
+
+    const h1Text = "Nesrine BEKKAR YAHOUM";
+    const h2Text = "développeur web - intégrateur";
+    const h1Delay = h1Text.length * speed;
+
+    this.setState({
+      h1Text: '',
+      h2Text: '',
+    });
+
+    setTimeout(() => {
+      this.typeEffect(h1Text, speed, 'h1Text');
+    }, 1000);
+    
+    setTimeout(()=> {
+      this.typeEffect(h2Text, speed, 'h2Text');
+    }, 1000 + h1Delay)
+
+
+    h1.classList.remove("hidden");
+
+
+    setTimeout(() => {
+      h2.classList.remove("hidden");
+    }, h1Delay);
+  }
+
+  render() {
+    return (
+      <div className='txt-display'>
+        <h1 ref={this.h1Ref} className='hidden'>{this.state.h1Text}</h1>
+        <h2 ref={this.h2Ref} className='hidden'>{this.state.h2Text}</h2>
+      </div>
+    )
+  }
+}
+  
+class AboutMe extends Component {
+  render() {
+    return (
+      <section>
+        <div id="about-me">
+          <div className="presentation">
+            <p>
+              Hello{' '}
+              <TxtRotate
+                toRotate='["World !", "Visiteur !", "Recruteur ?!"]'
+                period="3000"
+              />
+              <span className='wrap-border'>
+                |
+              </span>
+            </p>
+            <TxtToDisplay />
+            <button className='btn btn-about-me lightblue-btn'><a href='#profil'>Plus d'info</a></button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default AboutMe;
--- src/components/Profil/Profil.jsx
+++ src/components/Profil/Profil.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import monImage from "../../assets/images/bakground/imeneikhlef.webp";
-//import { ReactComponent as  } from "../../assets/images/bakground/Sun.svg";
+//import monImage from "../../assets/images/bakground/imeneikhlef.webp";
+//mport { ReactComponent as monImg } from "../../assets/images/bakground/imeneikhlef.webp";
 const Profil = () => {
   return (
     <div className="Profil__container">
      
-      <div className="Profil_Presentation">
+      <div className="Profil__info">
          
-        <p className="Piographie">
+        <p className="Profil__biography">
          
          Je suis Nesrine , je suis développeuse web frontend créative, autonome et passionnée par tous ce qui lien avec la technologie, l’informatique, web exactement la création de sites web et d’applications e-commerce, et même des refontes de site web, dotée d’une expérience dans différents secteurs d’activités.
          Après avoir obtenu un diplôme de licence BAC+4 en Marketing de l’école supérieur de commerce d’Alger,  j’ai commencé ma carrière professionnelle avec un poste de chargée clientèle que je l’ai occupé pendant 2 ans chez une entreprise de livraison en suite  j’ai une assistante de direction et juste après j’ai occupé un poste de téléconseillère pendant 3 ans au niveau du centre d’appel de MOBILIS le 1 er opérateur de téléphonie mobile en Algérie.
          </p>   
-   <div className="ImgeProfil">
+   {/* <div className="ImgeProfil">
         <img src="{monImage}" alt="image de portfolio Nes" />
-      </div>  
+      </div>   */}
 
+<monImg />
       </div>
     </div>
   );
