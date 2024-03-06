02d4a527bd07757ebacf2834c6307b3c8bf335da
NesrineYAH <nesrineyahoum@gmail.com>
2024-03-06 23:25:17 +0100

    //

--- src/pages/MentionsLegales/MentionsLegales.jsx
+++ src/pages/MentionsLegales/MentionsLegales.jsx
@@ -14,7 +14,8 @@ const MentionsLegales = () => {
 
 <span  style={{whiteSpace:"nowrap"}}>{t('TextInfo.pI')} : <span style={{fontWeight: 'bold'}}> {t('TextInfo.nom')}</span></span> <br/>
 <span >{t('TextInfo.pS')}<span style={{fontWeight: 'bold'}}>{t('TextInfo.pS1')}</span></span> <br/>
-<span>{t('TextInfo.pII')} <span style={{fontWeight: 'bold'}}> {t('TextInfo.nom')}</span> {t('TextInfo.pII')} <strong style={{fontWeight: 'bold'}}> contact@nesrinebekkar.com </strong></span>   <br/>
+<span>{t('TextInfo.pII')} <span style={{fontWeight: 'bold'}}> {t('TextInfo.nom')}</span></span><br/>
+ {t('TextInfo.pII1')} <strong style={{fontWeight: 'bold'}}> contact@nesrinebekkar.com </strong><br/>
 <span>{t('TextInfo.pS2')} :<span style={{fontWeight: 'bold'}}> {t('TextInfo.nom')}</span></span> <br/>
 <span>{t('TextInfo.pIII')} <span style={{fontWeight: 'bold'}}>{t('TextInfo.pIII1')}</span></span><br/>
 <p>{t('TextInfo.pr')}</p>
