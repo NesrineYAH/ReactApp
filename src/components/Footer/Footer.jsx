1e318c586c86f4d03f089cdb65a5580be913720d
NesrineYAH <nesrineyahoum@gmail.com>
2024-12-03 15:49:13 +0100

    //

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,9 +11,13 @@ export default function Footer() {
       <h2 className="footer__text-copyright" style={{ direction: lang === "ar" ? "rtl" : "ltr", alignItems:'center'}}>{t("footer")}</h2>
 
       <ul className='footer__link'>
-           <li><a href={`../../SiteMap/#sitemap`} target="_blank" rel="noopener noreferrer" className='TitreH3'>{t('plan')}</a></li>  
-       || <li><a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>{t('Mentions')}</a></li> 
-       </ul>
+           <li>
+            <a href={`../../SiteMap/#sitemap`} target="_blank" rel="noopener noreferrer" className='TitreH3'>{t('plan')}</a>
+            </li>  
+       || <li>
+        <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>{t('Mentions')}</a>
+        </li> 
+      </ul>
   
       <a className="fa" title="Go to top" href="#home">
       <FaChevronUp id="toTop "/>
--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -10,7 +10,7 @@ const lang = localStorage.getItem("i18nextLng");
 document.body.dir = i18n.dir();       
 
     return (
-        <div className='Language'>
+        <label className='Language'> lang
             <select  style={{ direction: lang === "ar" ? "rtl" : "ltr" }}
                       className="btn_lang"
                       onChange={(e) => i18n.changeLanguage(e.target.value)}
@@ -19,7 +19,7 @@ document.body.dir = i18n.dir();
                       <option value="en">en</option>
                       <option value="ar">ar</option>
              </select>
-        </div>
+        </label>
     );
 };
 
