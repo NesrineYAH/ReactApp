b32cb89a86067f7d39a92c5dc19d3839325e5297
NesrineYAH <nesrineyahoum@gmail.com>
2024-12-11 17:24:23 +0100

    Mise en place Pratique SEO  II

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Suspense  } from "react";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -10,6 +10,13 @@ import MentionLegales from './pages/MentionsLegales/MentionsLegales';
 import SiteMap from './pages/SiteMap/SiteMap';
 import { SyncLoader   }  from 'react-spinners'; 
 import { css } from "@emotion/react";
+
+const home = React.lazy(() => import("./pages/Home/Home"));
+const error = React.lazy(() => import("./pages/PageError/PageError"));
+const project = React.lazy(() => import("./pages/Project/Project"));
+const mentionLegales = React.lazy(() => import("./pages/MentionsLegales/MentionsLegales"));
+const siteMap = React.lazy(() => import("./pages/SiteMap/SiteMap"));
+
 
 class App extends React.Component {
   constructor(props) {
@@ -43,6 +50,7 @@ class App extends React.Component {
        <div className="main">
       <BrowserRouter>
        <Header handleClick={this.handleClick} />
+       <Suspense fallback={<div>Chargement en cours...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
@@ -50,6 +58,7 @@ class App extends React.Component {
           <Route path="/MentionLegales" element={<MentionLegales />} />
           <Route path="/SiteMap" element={<SiteMap />} />
         </Routes>
+        </Suspense>
       </BrowserRouter>  
      <Footer />
       </div>
