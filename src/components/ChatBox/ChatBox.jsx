8ee23387cef2b391e94c22d7544ac80af6326036
NesrineYAH <nesrineyahoum@gmail.com>
2025-08-24 22:26:22 +0200

    //

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -18,7 +18,14 @@ function ChatBox() {
 
   return (
     <div className="chatbox">
-        <i><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.583 14.894l-3.256 3.78c-.7.813-1.26.598-1.25-.46a10689.413 10689.413 0 0 1 .035-4.775V4.816a3.89 3.89 0 0 1 3.88-3.89h12.064a3.885 3.885 0 0 1 3.882 3.89v6.185a3.89 3.89 0 0 1-3.882 3.89H4.583z" fill="#FFF" fill-rule="evenodd"></path></svg></i>
+      
+<button class="bpw-widget-btn bpw-floating-button bpw-anim-undefined" aria-label="Ouvrir ou fermer la conversation">
+    <i>
+    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.583 14.894l-3.256 3.78c-.7.813-1.26.598-1.25-.46a10689.413 10689.413 0 0 1 .035-4.775V4.816a3.89 3.89 0 0 1 3.88-3.89h12.064a3.885 3.885 0 0 1 3.882 3.89v6.185a3.89 3.89 0 0 1-3.882 3.89H4.583z" fill="#FFF" fill-rule="evenodd">
+    </path>
+    </svg>
+    </i>
+ </button>
       <div className="chatbox__header">💬 Chat with Nesrine </div>
       <div className="chatbox__messages">
         {messages.map((msg, index) => (
