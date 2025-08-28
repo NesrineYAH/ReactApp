46dfb5d55fdf4fe6cfd4c2c9c719e522091c277a
NesrineYAH <nesrineyahoum@gmail.com>
2025-08-29 00:56:39 +0200

    modification mise en page

--- src/components/ChatWidget/ChatWidget.jsx
+++ src/components/ChatWidget/ChatWidget.jsx
@@ -15,15 +15,21 @@ function ChatWidget() {
         <i>
           {isOpen ? (
             // Icône quand chat ouvert (croix pour fermer)
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M18 6L6 18M6 6l12 12" stroke="white" strokeWidth="2" />
-            </svg>
+     <svg 
+  xmlns="http://www.w3.org/2000/svg" 
+  width="20" 
+  height="20" 
+  viewBox="0 0 24 24" 
+  fill="none" 
+  stroke="white" 
+  stroke-width="2" 
+  stroke-linecap="round" 
+  stroke-linejoin="round"
+>
+  <line x1="18" y1="6" x2="6" y2="18" />
+  <line x1="6" y1="6" x2="18" y2="18" />
+</svg>
+
           ) : (
             // Icône quand chat fermé (bulle de chat)
             <svg
