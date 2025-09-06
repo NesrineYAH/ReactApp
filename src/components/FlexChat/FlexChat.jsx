35f6b25ff55e0697e015d9bbc9574e4cca7af469
NesrineYAH <nesrineyahoum@gmail.com>
2025-09-06 16:11:03 +0200

    Intégration varriable d'environement

--- src/components/FlexChat/FlexChat.jsx
+++ src/components/FlexChat/FlexChat.jsx
@@ -1,13 +1,19 @@
+
 import { useEffect } from "react";
 
 const FlexChatWidget = () => {
   useEffect(() => {
     if (window.FlexChat) {
       new window.FlexChat({
-        REACT_APP_TWILIO_FLOW_SID: "REACT_APP_TWILIO_ACCOUNT_SID",
+        REACT_APP_TWILIO_ACCOUNT_SID:  "process.env.TWILIO_ACCOUNT_SID",
+        REACT_APP_TWILIO_FLOW_SID:  "process.env.TWILIO_FLOW_SID",
+         title: "💬 Chat avec Nesrine",
+        subtitle: "Pose-moi tes questions",
         themeColor: "#ff6b6b",
         position: "right",
       }).mount();
+    } else {
+      console.error("⚠️ FlexChat n'est pas chargé !");
     }
   }, []);
 
